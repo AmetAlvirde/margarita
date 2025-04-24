470156831ca30bf911a3881f4af0f99b524e2d0f
Amet Alvirde <Amet.Alvirde@gmail.com>
2025-04-24 12:32:38 -0300

    feat: Adds Tailwind base config to next.js app.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,16 @@
 import { Button } from "@repo/ui/button";
 import { ProjectCard } from "@repo/ui/components/project-card";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="max-w-screen-md mx-auto text-blue-500">
+      <main>
         <ProjectCard
           id="1"
           name="Project 1"
           description="Project 1 description"
         />
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
+        <Button appName="web">Open alert</Button>
       </main>
     </div>
   );
