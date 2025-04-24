a15147f3f20b9198c1fcad7d93c9504eaa3669d8
Amet Alvirde <Amet.Alvirde@gmail.com>
2025-04-23 22:18:33 -0300

    Adds basic internal package interaction.
    @repo/web uses @repo/ui and @repo/types
    @repo/ui uses @repo/types
    
    <ProjectCard /> component created to demonstrate those
    interactions.
    
    There is an issue on how @repo/web uses @repo/eslint-config.
    Fixed it extending the linting rules on @repo/web. Should be
    an issue to be solved fot the next round.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@repo/ui/button";
+import { ProjectCard } from "@repo/ui/project-card";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <ProjectCard
+          id="1"
+          name="Project 1"
+          description="Project 1 description"
+        />
         <Button appName="web" className={styles.secondary}>
           Open alert
         </Button>
